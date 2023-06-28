65490f5531afd31dc32e4e72ac2a10bcaba1f5d6
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-28 22:21:48 +0300

    Initial commit

--- src/Draft1_Task2_1_10.java
+++ src/Draft1_Task2_1_10.java
@@ -1,5 +1,14 @@
-public class Main {
+package lessons.java;
+
+
+import
+
+import java.util.ArrayList;.java.util.ArrayList;
+
+public class Draft1_Task2_1_10 {
     public static void main(String[] args) {
+
+        ArrayList<Product> catalog = ArrayList<>();
         System.out.println("Hello world!");
     }
 }
