ab1572eb84fa643cf9985a16bc89cd330552cdb6
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-29 01:18:51 +0300

    Initial commit

--- src/lessons/java/Draft1_Task2_1_10.java
+++ src/lessons/java/Draft1_Task2_1_10.java
@@ -7,13 +7,32 @@ public class Draft1_Task2_1_10 {
 
 //        Пример _ ППППППППППППППППППППППППППППППППППП
     public static void main(String[] args) {
-        System.out.println("Задание : \n1.\tПНеобходимо вывести большими буквами названия всех " +
+        System.out.println("Задание : \n1.\tНеобходимо вывести большими буквами названия всех " +
                 "\nпродуктов из магазина электроники, написанного на уроке.\n\nРешение: ");
             /*IntStream.range(0, 5).map(n -> n * n).forEach(a -> System.out.print(a + " "));*/
-            IntStream.range(0, 5).map(n -> n * n).forEach(System.out::println);
+            /*IntStream.range(0, 5).map(n -> n * n).forEach(System.out::println);*/
+            /*IntStream.range(-10, 10).map(x -> x * x - 5 * x - 10).filter(x -> x>0).forEach(System.out::println);*/
+            ArrayList<Product> catalog = new ArrayList<>();
+
+            catalog.add(new Product("Nokia 3310", 12345676));
+            catalog.add(new Product("Samsung Galaxy S100", 50304));
+            catalog.add(new Product("IPhone 20", 392049));
+            catalog.add(new Product("Google Pixel 10a", 203421));
+            catalog.stream().map((product -> product.name)).forEach(x -> System.out.println(x.toUpperCase()));
+
+    }
+
+    //x^-5x-10
+}
+class Product {
+    String name;
+    int price;
+
+    public Product(String name, int price) {
+        this.name = name;
+        this.price = price;
     }
 }
-
 //        Конец Примера _ КККККККККККККККК
 
 
@@ -21,23 +40,41 @@ public class Draft1_Task2_1_10 {
 
 
 
-////        Пример _ ППППППППППППППППППППППППППППППППППП
+////        Пример 1 ППППППППППППППППППППППППППППППППППП
 //    public static void main(String[] args) {
-//        System.out.println("Задание : \n1.\tПНеобходимо вывести большими буквами названия всех " +
+//        System.out.println("Задание : \n1.\tНеобходимо вывести большими буквами названия всех " +
 //                "\nпродуктов из магазина электроники, написанного на уроке.\n\nРешение: ");
+//            /*IntStream.range(0, 5).map(n -> n * n).forEach(a -> System.out.print(a + " "));*/
+//            /*IntStream.range(0, 5).map(n -> n * n).forEach(System.out::println);*/
+//            /*IntStream.range(-10, 10).map(x -> x * x - 5 * x - 10).filter(x -> x>0).forEach(System.out::println);*/
+//            ArrayList<Product> catalog = new ArrayList<>();
 //
-//        ArrayList<Product> catalog = ArrayList<>();
-//        catalog.add(new Product("Nokia 3310, 12345676"));
-//        catalog.add(new Product("Samsung Galaxy S100, 50304"));
-//        catalog.add(new Product("IPhone 29, 392049"));
-//        catalog.add(new Product("Google Pixel 10a, 203421"));
-//        catalog.stream().map((product -> product.price)).forEach(System.out::println);
+//            catalog.add(new Product("Nokia 3310", 12345676));
+//            catalog.add(new Product("Samsung Galaxy S100", 50304));
+//            catalog.add(new Product("IPhone 20", 392049));
+//            catalog.add(new Product("Google Pixel 10a", 203421));
+//            catalog.stream().map((product -> product.name + " " + product.price)).forEach(System.out::println);
+//
+//    }
+//
+//    //x^-5x-10
+//}
+//class Product {
+//    String name;
+//    int price;
+//
+//    public Product(String name, int price) {
+//        this.name = name;
+//        this.price = price;
 //    }
 //}
-//
-//class Product {
-//}
-////        Конец Примера _ КККККККККККККККК
+////        Конец Примера 1 КККККККККККККККК
+
+
+
+
+
+
 
 
 
